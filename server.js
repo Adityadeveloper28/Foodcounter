7f597b6ecca672b0cee82d169ffbb9d49a9426e8
Aditya Singh <singhadi280303@gmail.com>
2023-12-26 17:17:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ require("dotenv").config();
 const mongoURI = process.env.MONGO_URL;
 
 const corsOptions = {
-  origin: "https://foodcounter-frontend.vercel.app/", // Replace with the origin of your frontend
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'https://foodcounter-frontend.vercel.app',
+      'https://foodcounter-frontend-2nwyy7f3r-aditya-singhs-projects-5f3944b5.vercel.app',
+      // Add any other allowed origins as needed
+    ];
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   optionsSuccessStatus: 200,
 };
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
